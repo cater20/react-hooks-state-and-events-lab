ab42fdbce94de018f8b3335abfecc1d9012a542e
chrisphine <onyangoketta@gmail.com>
2022-07-16 12:20:24 +0300

    onclick function

--- src/components/App.js
+++ src/components/App.js
@@ -6,13 +6,17 @@ function App() {
 
   // replace 'false' with a state variable that can be toggled between true and false
   // this will be used for the Dark Mode Toggle feature
-  const appClass = false ? "App dark" : "App light"
 
-  return (
+ const appClass = false ? "App dark" : "App light"
+const [darkMode ,setDarkMode  ]=useState(false)
+
+function onClick(){}
+
+   return (
     <div className={appClass}>
       <header>
         <h2>Shopster</h2>
-        <button>Dark Mode</button>
+        <button onClick={handleclick}>{darkMode ? "Dark" : "light"}</button>
       </header>
       <ShoppingList items={itemData} />
     </div>
